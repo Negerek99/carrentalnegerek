a059258ff25dcd1d30e194ca861d67a47c795777
Negerek99 <98485135+Negerek99@users.noreply.github.com>
2022-02-04 11:43:33 +0100

    Add files via upload

---
+++ admin/includes/header.php
@@ -0,0 +1,13 @@
+<div class="brand clearfix">
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+			
+			<li class="ts-account">
+				<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> Admin <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="change-password.php">Zmien hasło</a></li>
+					<li><a href="logout.php">Wyloguj</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
---
+++ admin/includes/leftbar.php
@@ -0,0 +1,23 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Menu</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Panel</a></li>	
+				<li><a href="#"><i class="fa fa-files-o"></i> Marka</a>
+					<ul>
+						<li><a href="create-brand.php"> Dodaj marke</a></li>
+						<li><a href="manage-brands.php"> Edytuj marke</a></li>
+					</ul>
+				</li>
+				<li><a href="#"><i class="fa fa-sitemap"></i> Pojazdy</a>
+					<ul>
+						<li><a href="post-avehical.php"> Dodaj pojazd</a></li>
+						<li><a href="manage-vehicles.php"> Edytuj pojazdy</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Rezerwacje</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i> Pytania</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Zarejestrowani użytkownicy</a></li>
+				<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Edytuj kontakt</a></li>
+			</ul>
+		</nav>
